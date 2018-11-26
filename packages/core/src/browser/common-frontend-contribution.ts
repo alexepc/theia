aaecf7f10570b2cdcf35203a847b8496f550002d
Paul Maréchal <paul.marechal@ericsson.com>
2018-11-26 16:10:30 -0500

    [core] Add a handler preventing exit if dirty
    Add one contribution to the beforeUnload hook to prevent a Theia application
    from exiting if some shell widgets are dirty.
    
    Signed-off-by: Paul Maréchal <paul.marechal@ericsson.com>

--- packages/core/src/browser/common-frontend-contribution.ts
+++ packages/core/src/browser/common-frontend-contribution.ts
@@ -17,6 +17,7 @@
 import { injectable, inject } from 'inversify';
 import { MAIN_MENU_BAR, MenuContribution, MenuModelRegistry } from '../common/menu';
 import { KeybindingContribution, KeybindingRegistry } from './keybinding';
+import { FrontendApplicationContribution, OnWillStopEvent } from './frontend-application';
 import { CommandContribution, CommandRegistry, Command } from '../common/command';
 import { UriAwareCommandHandler } from '../common/uri-command-handler';
 import { SelectionService } from '../common/selection-service';
@@ -187,7 +188,7 @@ export const supportCopy = browser.isNative || document.queryCommandSupported('c
 export const supportPaste = browser.isNative || (!browser.isChrome && document.queryCommandSupported('paste'));
 
 @injectable()
-export class CommonFrontendContribution implements MenuContribution, CommandContribution, KeybindingContribution {
+export class CommonFrontendContribution implements FrontendApplicationContribution, MenuContribution, CommandContribution, KeybindingContribution {
 
     constructor(
         @inject(ApplicationShell) protected readonly shell: ApplicationShell,
@@ -510,5 +511,14 @@ export class CommonFrontendContribution implements MenuContribution, CommandCont
 
     protected async openAbout() {
         this.aboutDialog.open();
+    }
+
+    onWillStop(event: OnWillStopEvent) {
+        if (this.shell.canSaveAll()) {
+            event.preventStop();
+            event.onStay(() => {
+                this.messageService.info('Some documents should be saved, data will be lost otherwise.');
+            });
+        }
     }
 }
